b4c83e9edc3f57cb7a325822d3d5466ce60f45d1
CriminalTalent <rntjdrud@naver.com>
2026-06-12 10:54:14 +0900

    Create Mastodon_client.rb

---
+++ Mastodon_client.rb
@@ -0,0 +1,52 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
+class MastodonClient
+  def initialize(base_url, token)
+    @base_url = base_url
+    @token = token
+  end
+
+  def post_public(text)
+    uri = URI("#{@base_url}/api/v1/statuses")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    req = Net::HTTP::Post.new(uri)
+    req['Authorization'] = "Bearer #{@token}"
+    req['Content-Type'] = 'application/json'
+    req.body = { status: text, visibility: 'public' }.to_json
+
+    res = http.request(req)
+    data = JSON.parse(res.body)
+    puts "[Mastodon] 툿 전송 완료: #{data['id']}"
+    data['id']
+  rescue => e
+    puts "[Mastodon 오류] #{e.message}"
+    nil
+  end
+
+  def reply_public(text, in_reply_to_id)
+    uri = URI("#{@base_url}/api/v1/statuses")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    req = Net::HTTP::Post.new(uri)
+    req['Authorization'] = "Bearer #{@token}"
+    req['Content-Type'] = 'application/json'
+    req.body = {
+      status: text,
+      visibility: 'public',
+      in_reply_to_id: in_reply_to_id
+    }.to_json
+
+    res = http.request(req)
+    data = JSON.parse(res.body)
+    puts "[Mastodon] 스레드 툿 전송 완료: #{data['id']}"
+    data['id']
+  rescue => e
+    puts "[Mastodon 오류] #{e.message}"
+    nil
+  end
+end
