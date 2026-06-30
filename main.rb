a23d2ecd603f6b807dba259a248b9820eff8f715
CriminalTalent <rntjdrud@naver.com>
2026-06-30 21:54:40 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-# main.rb
+# main.rb (마스토돈 커맨드 방식)
 $stdout.sync = true
 $stderr.sync = true
 
@@ -10,51 +10,43 @@ require_relative 'sheet_manager'
 require_relative 'mastodon_client'
 require_relative 'battle_processor'
 require_relative 'toot_builder'
+require_relative 'state_manager'
 
-OPS_SHEET_ID      = ENV['OPS_SHEET_ID']
 RUNNER_SHEET_ID   = ENV['RUNNER_SHEET_ID']
 CREATURE_SHEET_ID = ENV['CREATURE_SHEET_ID']
 VIEW_SHEET_ID     = ENV['VIEW_SHEET_ID']
-RUNNER_SHEET_URL  = ENV['RUNNER_SHEET_URL']
 CREDENTIALS_PATH  = File.join(__dir__, 'credentials.json')
 POLL_INTERVAL     = 30
-PHASE_FILE        = File.join(__dir__, 'battle_phase.json')
 
-def read_phase
-  return nil unless File.exist?(PHASE_FILE)
-  JSON.parse(File.read(PHASE_FILE), symbolize_names: true)
-rescue
-  nil
-end
+def setup_battle(round, mastodon, runner_sheet)
+  base_stats = runner_sheet.read_runner_stats
+  skill_data = runner_sheet.read_skill_data
 
-def write_phase(data)
-  File.write(PHASE_FILE, JSON.pretty_generate(data))
-end
-
-def clear_phase
-  File.delete(PHASE_FILE) if File.exist?(PHASE_FILE)
-end
-
-def announce_round(trigger, mastodon)
-  round = trigger[:round]
+  a_state = runner_sheet.read_runner_state
   
   mastodon.post_public(
     "[#{round}라운드 시작]\n\n" \
-    "A팀은 아래 시트의 그리드에 자신의 이름을 입력해주세요.\n" \
-    "#{RUNNER_SHEET_URL}\n\n" \
+    "A팀은 자동봇 시트의 그리드에 자신의 이름을 입력해주세요.\n" \
     "위치: A1~H8 중 선택\n" \
     "크리쳐는 자동으로 공격합니다."
   )
 
-  puts "[전투봇] #{round}라운드 알림 완료"
+  {
+    round: round,
+    status: 'waiting_commands',
+    base_stats: base_stats,
+    skill_data: skill_data,
+    runner_state: a_state
+  }
 end
 
-def settle_round(phase, ops_sheet, runner_sheet, creature_sheet, view_sheet, mastodon)
-  round = phase[:round]
+def settle_battle(state, mastodon, runner_sheet, creature_sheet, view_sheet, state_mgr)
+  round = state[:round]
   puts "[전투봇] #{round}라운드 전체 정산 시작"
 
-  base_stats = ops_sheet.read_base_stats
-  a_state    = runner_sheet.read_runner_state
+  base_stats = state[:base_stats]
+  skill_data = state[:skill_data]
+  a_state = runner_sheet.read_runner_state
   
   creature_config = creature_sheet.read_creature_config
   creature_name = creature_config&.dig(:name)
@@ -84,10 +76,9 @@ def settle_round(phase, ops_sheet, runner_sheet, creature_sheet, view_sheet, mas
     }
   end
 
-  skill_data  = ops_sheet.read_skill_data
-  corrections = ops_sheet.read_corrections
-  cooldowns   = ops_sheet.read_cooldowns
-  buffs_in    = ops_sheet.read_buffs
+  cooldowns = state_mgr.read_cooldowns
+  buffs_in  = state_mgr.read_buffs
+  corrections = []
 
   puts "[전투봇] A팀 #{a_state.size}명 / 크리쳐 #{creature_name}"
 
@@ -98,61 +89,7 @@ def settle_round(phase, ops_sheet, runner_sheet, creature_sheet, view_sheet, mas
 
   log, updated_states, updated_cooldowns, updated_buffs = processor.process
 
-  ops_sheet.clear_corrections
-  ops_sheet.write_cooldowns(updated_cooldowns)
-  ops_sheet.write_buffs(updated_buffs)
+  state_mgr.write_cooldowns(updated_cooldowns)
+  state_mgr.write_buffs(updated_buffs)
 
-  a_updated = updated_states.select { |name, _| a_state.any? { |s| s[:name] == name } }.values
-  creature_updated = updated_states[creature_name]
-
-  runner_sheet.update_runner_state(a_updated)
-  creature_sheet.update_creature_state(creature_updated)
-  view_sheet.update_view_map(a_updated + [creature_updated])
-  view_sheet.update_view_team(a_updated, 'A팀')
-  view_sheet.update_view_creature(creature_updated)
-
-  puts "[전투봇] 현황 + 맵 + 쿨타임 + 버프 업데이트 완료"
-
-  toots = TootBuilder.new(round, log).build
-  puts "[전투봇] 툿 #{toots.size}개 생성"
-
-  parent_id = nil
-  toots.each_with_index do |text, i|
-    sleep(1)
-    parent_id = i == 0 ? mastodon.post_public(text) : mastodon.reply_public(text, parent_id)
-  end
-
-  clear_phase
-  puts "[전투봇] #{round}라운드 전체 정산 완료"
-end
-
-def run_once(ops_sheet, runner_sheet, creature_sheet, view_sheet, mastodon)
-  trigger = ops_sheet.read_trigger
-  return unless trigger&.dig(:on)
-
-  phase = read_phase
-
-  if phase && phase[:status] == 'waiting_commands'
-    ops_sheet.turn_off_trigger
-    settle_round(phase, ops_sheet, runner_sheet, creature_sheet, view_sheet, mastodon)
-  else
-    announce_round(trigger, mastodon)
-    write_phase({ round: trigger[:round], status: 'waiting_commands' })
-  end
-rescue => e
-  puts "[전투봇 오류] #{e.class}: #{e.message}"
-  puts e.backtrace.first(8)
-end
-
-puts "[전투봇] 시작"
-
-ops_sheet      = SheetManager.new(OPS_SHEET_ID, CREDENTIALS_PATH)
-runner_sheet   = SheetManager.new(RUNNER_SHEET_ID, CREDENTIALS_PATH)
-creature_sheet = SheetManager.new(CREATURE_SHEET_ID, CREDENTIALS_PATH)
-view_sheet     = SheetManager.new(VIEW_SHEET_ID, CREDENTIALS_PATH)
-mastodon       = MastodonClient.new(ENV['MASTODON_BASE_URL'], ENV['BATTLE_TOKEN'])
-
-loop do
-  run_once(ops_sheet, runner_sheet, creature_sheet, view_sheet, mastodon)
-  sleep(POLL_INTERVAL)
-end
+  a_updated = updated_states.select { |name,
